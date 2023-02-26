55ade470cae5a67da3c545fdeedde28e139fdab1
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-02-26 21:20:04 +0100

    reverse string js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let splitString = string.split('')
+
+    let reverseString = splitString.reverse();
+
+    let joinString = reverseString.join("");
+
+    return joinString;
+
+
+
+
 
 };
 
