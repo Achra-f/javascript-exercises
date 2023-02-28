b5da1576b1ac1d68d11e8ab6a1dac29293e72b70
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-02-28 16:40:50 +0100

    temp conversion js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let ctemp = 0;
+  let rounded = 0;
+  ctemp = (temp - 32) * 5 / 9;
+
+  rounded = Math.round(ctemp * 10) / 10;
+
+  return rounded;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let ftemp = 0;
+  let rounded = 0;
+  ftemp = (temp * 9 / 5) + 32;
+
+  rounded = Math.round(ftemp * 10) / 10;
+
+  return rounded;
 };
 
 // Do not edit below this line
